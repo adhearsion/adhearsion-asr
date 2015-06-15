005a6e48e48b55705d3e923d9e40594fe9e9a28d
Ben Langfeld <ben@langfeld.me>
2015-06-15 12:02:18 -0300

    Avoid RSpec deprecations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,14 +2,14 @@ require 'adhearsion'
 require 'adhearsion-asr'
 
 RSpec.configure do |config|
-  config.color_enabled = true
+  config.color = true
   config.tty = true
 
   config.mock_with :rspec
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
 
-  config.backtrace_clean_patterns = [/rspec/]
+  config.backtrace_exclusion_patterns = [/rspec/]
 
   config.before do
     @current_datetime = DateTime.now
